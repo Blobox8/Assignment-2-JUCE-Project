32ed5bb430486f8a9750c5c484acb3245bbebeb7
Albaraa <gsama6981@gmail.com>
2025-10-20 22:39:59 +0300

    Modular design
    Buttons are located in PlayerGUI.cpp

---
+++ PlayerAudio.cpp
@@ -0,0 +1,49 @@
+#include "PlayerAudio.h"
+
+// constructor
+PlayerAudio::PlayerAudio() {
+    formatManager.registerBasicFormats();
+}
+
+// destructor
+PlayerAudio::~PlayerAudio() {
+    transportSource.setSource(nullptr);
+}
+
+void PlayerAudio::prepareToPlay(int samplesPerBlockExpected, double sampleRate)
+{
+    transportSource.prepareToPlay(samplesPerBlockExpected, sampleRate);
+}
+
+void PlayerAudio::getNextAudioBlock(const juce::AudioSourceChannelInfo& bufferToFill)
+{
+    transportSource.getNextAudioBlock(bufferToFill);
+}
+
+void PlayerAudio::releaseResources()
+{
+    transportSource.releaseResources();
+}
+
+bool PlayerAudio::loadFile(const juce::File& file) {
+    if (file.existsAsFile())
+    {
+        if (auto* reader = formatManager.createReaderFor(file))
+        {
+            // 🔑 Disconnect old source first
+            transportSource.stop();
+            transportSource.setSource(nullptr);
+            readerSource.reset();
+
+            // Create new reader source
+            readerSource = std::make_unique<juce::AudioFormatReaderSource>(reader, true);
+
+            // Attach safely
+            transportSource.setSource(readerSource.get(),
+                0,
+                nullptr,
+                reader->sampleRate);
+            transportSource.start();
+        }
+    }
+}
---
+++ PlayerGUI.h
@@ -0,0 +1,43 @@
+#pragma once
+#include <JuceHeader.h>
+#include "PlayerAudio.h"
+
+
+class PlayerGUI : public juce::Component, 
+	public juce::Button::Listener, 
+	public juce::Slider::Listener 
+{
+public:
+	PlayerGUI();
+	~PlayerGUI();
+
+	void prepareToPlay(int samplesPerBlockExpected, double sampleRate);
+	void getNextAudioBlock(const juce::AudioSourceChannelInfo& bufferToFill);
+	void releaseResources();
+
+	// gui
+	void paint(juce::Graphics& g) override;
+	void resized() override;
+
+	
+
+private:
+	PlayerAudio playeraudio;
+
+	juce::TextButton loadButton{ "Load Files" };
+	juce::TextButton restartButton{ "Restart" };
+	juce::TextButton stopButton{ "Stop" };
+
+	// loop button
+	juce::TextButton loopButton{ "loop" };
+
+	juce::Slider volumeSlider;
+
+	std::unique_ptr<juce::FileChooser> fileChooser;
+
+	// Event
+	void buttonClicked(juce::Button* button) override;
+	void sliderValueChanged(juce::Slider* slider) override;
+
+	JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(PlayerGUI);
+};
