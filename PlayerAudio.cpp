fba59151f5fbd643bf76bf7522344b5d9400cb28
Blobox8 <gsama6981@gmail.com>
2025-10-24 18:49:00 +0300

    add task 2 buttons

--- PlayerAudio.cpp
+++ PlayerAudio.cpp
@@ -28,6 +28,18 @@ void PlayerAudio::releaseResources()
 void PlayerAudio::toggleLoop(std::unique_ptr<juce::AudioFormatReaderSource> &readersource) {
     readersource->setLooping(isLoop);
 }
+void PlayerAudio::gotostart()
+{
+	transportSource.setPosition(0.0);
+}
+
+void PlayerAudio::gotoend()
+{
+	double length = transportSource.getLengthInSeconds();
+
+	if (length > 0.0)
+		transportSource.setPosition(length);
+}
 
 bool PlayerAudio::loadFile(const juce::File& file) {
     if (file.existsAsFile())
