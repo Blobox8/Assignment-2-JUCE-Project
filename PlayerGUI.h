d7e4cfe81c88bc1af3c7d0569311734ca54c1946
Mahmoud Ahmed <20242307@stud.fci-cu.edu.eg>
2025-10-31 22:24:31 +0200

    Update PlayerGUI.h

--- PlayerGUI.h
+++ PlayerGUI.h
@@ -2,11 +2,11 @@
 #include <JuceHeader.h>
 #include "PlayerAudio.h"
 
-
-class PlayerGUI : public juce::Component, 
-	public juce::Button::Listener, 
+class PlayerGUI : public juce::Component,
+	public juce::Button::Listener,
 	public juce::Slider::Listener,
 	public juce::Timer
+
 {
 public:
 	PlayerGUI();
@@ -16,14 +16,12 @@ public:
 	void getNextAudioBlock(const juce::AudioSourceChannelInfo& bufferToFill);
 	void releaseResources();
 
-	// timer
-	void timerCallback() override;
-
 	// gui
 	void paint(juce::Graphics& g) override;
 	void resized() override;
 
-	
+	// Timer callback for updating progress
+	void timerCallback() override;
 
 private:
 	PlayerAudio playeraudio;
@@ -41,28 +39,27 @@ private:
 
 	// pause/end buttons
 	juce::TextButton pauseButton{ "pause" };
-	juce::TextButton toEndButton{ "to end" };
-
-	// segment loop toggle button
-	juce::ToggleButton segmentButton{ "segment loop" };
-
- 
 	juce::TextButton gotoendButton{ "gotoend" };
 	juce::TextButton gotostartButton{ "gotostart" };
 
+	//volume label
 	juce::Label volumeLabel;
 	juce::Slider volumeSlider;
-	juce::Slider posSlider;
-	juce::Slider segA, segB;
+
+	//speed label
+	juce::Slider speedSlider;
+	juce::Label speedLabel;
+
+	// Progress slider - ADD THIS
+	juce::Slider progressSlider;
+	juce::Label progressLabel;
 
 	std::unique_ptr<juce::FileChooser> fileChooser;
-
-
-	bool isMoved;
 
 	// Event
 	void buttonClicked(juce::Button* button) override;
 	void sliderValueChanged(juce::Slider* slider) override;
 
+	bool keyPressed(const juce::KeyPress& key) override;
 	JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(PlayerGUI);
 };
