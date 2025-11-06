f26708d5c83e5b7e346b6ede83db02eb81104be4
Mahmoud Ahmed <20242307@stud.fci-cu.edu.eg>
2025-11-06 22:22:18 +0200

    Update MainComponent.cpp

--- MainComponent.cpp
+++ MainComponent.cpp
@@ -2,17 +2,12 @@
 #include "PlayerAudio.h"
 #include "PlayerGUI.h"
 
-
 MainComponent::MainComponent()
-{   
+{
     addAndMakeVisible(player1);
-    addChildComponent(player2); // player2 is added but not visible
+    addChildComponent(player2);
 
-    
-
-    // mix
     player1.addMixerInputSource(player1, player2);
-    
 
     setSize(500, 250);
     setAudioChannels(0, 2);
@@ -23,14 +18,13 @@ MainComponent::~MainComponent()
     shutdownAudio();
 }
 
-
 void MainComponent::prepareToPlay(int samplesPerBlockExpected, double sampleRate)
 {
     player1.mixerPrepareToPlay(samplesPerBlockExpected, sampleRate);
 }
 
 void MainComponent::getNextAudioBlock(const juce::AudioSourceChannelInfo& bufferToFill)
-{           
+{
     player1.mixerGetNextAudioBlock(bufferToFill);
 }
 
@@ -40,8 +34,8 @@ void MainComponent::releaseResources()
     player2.releaseResources();
 }
 
-void MainComponent::resized() 
+void MainComponent::resized()
 {
-    player1.setBounds(10, 10, getWidth() - 20, getHeight()-20);
-    player2.setBounds(10, 300, getWidth() - 20, getHeight()-310);
+    player1.setBounds(10, 10, getWidth() - 20, getHeight() - 20);
+    player2.setBounds(10, 300, getWidth() - 20, getHeight() - 310);
 }
