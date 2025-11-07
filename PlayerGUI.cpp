5f722bec28c4275191dddad9492dc241ac57cf4d
Mahmoud Ahmed <20242307@stud.fci-cu.edu.eg>
2025-11-07 08:06:47 +0200

    Update PlayerGUI.cpp

--- PlayerGUI.cpp
+++ PlayerGUI.cpp
@@ -1,3 +1,4 @@
+
 #include "PlayerGUI.h"
 
 int PlayerGUI::numOfPlayers = 1;
@@ -18,6 +19,21 @@ PlayerGUI::PlayerGUI() :
 
     segmentButton.addListener(this);
     addAndMakeVisible(segmentButton);
+
+    nextButton.setColour(juce::TextButton::buttonColourId, juce::Colours::green);
+    nextButton.setColour(juce::TextButton::textColourOffId, juce::Colours::white);
+    prevButton.setColour(juce::TextButton::buttonColourId, juce::Colours::deepskyblue);
+    prevButton.setColour(juce::TextButton::textColourOffId, juce::Colours::white);
+    stopButton.setColour(juce::TextButton::buttonColourId, juce::Colours::red);
+    stopButton.setColour(juce::TextButton::textColourOffId, juce::Colours::white);
+
+    volumeSlider.setColour(juce::Slider::thumbColourId, juce::Colours::orange);
+    volumeSlider.setColour(juce::Slider::trackColourId, juce::Colours::white);
+    volumeSlider.setColour(juce::Slider::backgroundColourId, juce::Colours::darkgrey);
+
+    speedSlider.setColour(juce::Slider::thumbColourId, juce::Colours::orange);
+    speedSlider.setColour(juce::Slider::trackColourId, juce::Colours::white);
+    speedSlider.setColour(juce::Slider::backgroundColourId, juce::Colours::darkgrey);
 
     if (numOfPlayers == 1)
     {
@@ -123,7 +139,9 @@ void PlayerGUI::timerCallback()
 
 void PlayerGUI::paint(juce::Graphics& g)
 {
-    g.fillAll(juce::Colours::darkgrey);
+    g.fillAll(juce::Colours::transparentBlack);
+    g.setColour(juce::Colours::white.withAlpha(0.3f));
+    g.drawRoundedRectangle(getLocalBounds().toFloat().reduced(2), 10.0f, 2.0f);
 }
 
 void PlayerGUI::resized()
@@ -139,79 +157,81 @@ void PlayerGUI::resized()
 
     int y = 10;
 
-    // First row of buttons
+    // First column of buttons
     int buttonSpacing = (numOfPlayers == 2) ? 70 : 100;
 
-    loadMultiButton.setBounds(10, y, 90, buttonHeight);
-    restartButton.setBounds(110, y, 70, buttonHeight);
-    stopButton.setBounds(190, y, 70, buttonHeight);
-    loopButton.setBounds(270, y, 70, buttonHeight);
-    
+    loadMultiButton.setBounds(10, 10, 90, buttonHeight);
+    restartButton.setBounds(10, 60, 70, buttonHeight);
+    stopButton.setBounds(10, 110, 70, buttonHeight);
+    loopButton.setBounds(10, 160, 70, buttonHeight);
 
-    y += buttonHeight + 10;
 
-    // Second row of buttons
-    pauseButton.setBounds(10, y, 90, buttonHeight);
-    gotoendButton.setBounds(110, y, 70, buttonHeight);
-    gotostartButton.setBounds(190, y, 70, buttonHeight);
-    muteButton.setBounds(270, y, 70, buttonHeight);
-    segmentButton.setBounds(350, y, 70, buttonHeight);
+    //y += buttonHeight + 10;
+
+    // Second column of buttons
+    pauseButton.setBounds(getWidth() - 80, 10, 70, buttonHeight);
+    gotoendButton.setBounds(getWidth() - 80, 60, 70, buttonHeight);
+    gotostartButton.setBounds(getWidth() - 80,110, 70, buttonHeight);
+    muteButton.setBounds(getWidth() - 80, 160, 70, buttonHeight);
+
+
 
     y += buttonHeight + 10;
 
     // Navigation buttons
-    prevButton.setBounds(10, y, 80, smallButtonHeight);
-    nextButton.setBounds(100, y, 80, smallButtonHeight);
+    prevButton.setBounds(getWidth()/2 -80, getHeight() - 120, 80, 40);
+    nextButton.setBounds(getWidth() / 2 +20, getHeight() - 120, 80, 40);
 
     y += smallButtonHeight + 10;
 
     // File info
-    fileInfoLabel.setBounds(10, y, availableWidth - 20, labelHeight);
+    fileInfoLabel.setBounds(10, getHeight()-150, availableWidth - 20, labelHeight);
 
     y += labelHeight + 10;
+    segmentButton.setBounds(10, 240, 70, buttonHeight);
 
     // Segment controls
-    startLabel.setBounds(10, y, 40, labelHeight);
-    pointA.setBounds(55, y, 80, labelHeight);
-    endLabel.setBounds(145, y, 40, labelHeight);
-    pointB.setBounds(190, y, 80, labelHeight);
+    startLabel.setBounds(getWidth()/2 -110, 250, 40, labelHeight);
+    pointA.setBounds(getWidth() / 2 -70 , 250, 80, labelHeight);
+    endLabel.setBounds(getWidth() / 2 +10, 250, 40, labelHeight);
+    pointB.setBounds(getWidth() / 2  +50, 250, 80, labelHeight);
 
     y += labelHeight + 15;
 
     // Volume and speed control
     int controlWidth = (availableWidth - 30) / 2;
 
-    volumeLabel.setBounds(10, y, 50, labelHeight);
-    volumeSlider.setBounds(65, y, controlWidth - 60, sliderHeight);
+    volumeLabel.setBounds(10, 280, 50, labelHeight);
+    volumeSlider.setBounds(65, 280, controlWidth - 60, sliderHeight);
 
-    speedLabel.setBounds(controlWidth + 10, y, 50, labelHeight);
-    speedSlider.setBounds(controlWidth + 65, y, controlWidth - 60, sliderHeight);
+    speedLabel.setBounds(controlWidth + 10, 280, 50, labelHeight);
+    speedSlider.setBounds(controlWidth + 65, 280, controlWidth - 60, sliderHeight);
 
     y += sliderHeight + 15;
 
     // Calculate playlist height dynamically
-    int progressSectionHeight = 60;
-    int minPlaylistHeight = 80;
+    /*int progressSectionHeight = 60;
+    int minPlaylistHeight = 80;*/
 
-    int playlistHeight = availableHeight - y - progressSectionHeight;
+    int playlistHeight = 200;
 
     // Ensure minimum playlist height
-    if (playlistHeight < minPlaylistHeight) {
+   /* if (playlistHeight < minPlaylistHeight) {
         playlistHeight = minPlaylistHeight;
-    }
+    }*/
 
     // Position playlist
-    playlistBox.setBounds(10, y, availableWidth - 20, playlistHeight);
+    playlistBox.setBounds(10, getHeight()-350, availableWidth - 20, playlistHeight);
 
     y += playlistHeight + 10;
 
     // Progress slider
-    progressLabel.setBounds(10, y, 60, labelHeight);
-    progressSlider.setBounds(75, y, availableWidth - 90, sliderHeight);
+    progressLabel.setBounds(10, getHeight() - 50, 60, labelHeight);
+    progressSlider.setBounds(75, getHeight() - 50, availableWidth - 90, sliderHeight);
 
     // New track button only for first player
     if (numOfPlayers == 1) {
-        newTrackButton.setBounds(availableWidth - 120, 10, 110, 30);
+        newTrackButton.setBounds(getWidth()/2 - 50, 10, 110, 30);
     }
 }
 
